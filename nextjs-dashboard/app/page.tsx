ec0d128f593e01e5f4afda5267326459b213ad60
Mauricio <maugva05@hotmail.com>
2024-06-28 17:37:39 -0600

    preview

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -1,5 +1,8 @@
 import AcmeLogo from '@/app/ui/acme-logo';
-import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import { HomeIcon } from '@heroicons/react/24/outline';
+import { ShoppingCartIcon } from '@heroicons/react/24/outline';
+import { TruckIcon } from '@heroicons/react/24/outline';
+import { PhoneIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
@@ -8,42 +11,59 @@ import Image from 'next/image';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-      <AcmeLogo />
+      <div className="flex h-10 shrink-0 items-end rounded-lg bg-brown-300 p-10 md:h-40">
+        <AcmeLogo />
+        <Link
+            href="/dashboard"
+            className="flex items-center gap-5 rounded-lg bg-brown-700 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-brown-100 md:text-base ml-20"
+            >
+            <span>Inicio</span> <HomeIcon className="w-5 md:w-6" />
+          </Link>
+          <Link
+            href="/dashboard/catalogue"
+            className="flex items-center gap-5 rounded-lg bg-brown-700 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-brown-100 md:text-base ml-20"
+            >
+            <span>Productos</span> <ShoppingCartIcon className="w-5 md:w-6" />
+          </Link>
+          <Link
+            href="/dashboard/deliveries"
+            className="flex items-center gap-5 rounded-lg bg-brown-700 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-brown-100 md:text-base ml-20"
+            >
+            <span>Entregas</span> <TruckIcon className="w-5 md:w-6" />
+          </Link>
+          <Link
+            href="/dashboard/contact"
+            className="flex items-center gap-5 rounded-lg bg-brown-700 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-brown-100 md:text-base ml-20"
+            >
+            <span>Contacto</span> <PhoneIcon className="w-5 md:w-6" />
+          </Link>
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-        <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>          
-            <p className={styles.shape} />
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
-        </p>
-          <Link
-            href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-          >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
-          </Link>
+        <div className="flex flex-col justify-center gap-6 rounded-lg bg-brown-100 px-6 py-10 md:w-2/5 md:px-20">
+          <div className={styles.cup}>
+            <div className={styles.handle}/>
+          </div>
+          <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+            <strong>Bienvenido a Pasión Cafeto.</strong> Somos una empresa que
+            busca distribuir café con una calidez y atención distintiva.
+          </p>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-        <Image
-        src="/hero-desktop.png"
-        width={1000}
-        height={760}
-        className="hidden md:block"
-        alt="Screenshots of the dashboard project showing desktop version"
-      />     
-      <Image
-        src="/hero-mobile.png"
-        width={560}
-        height={620}
-        className="block md:hidden"
-        alt="Screenshot of the dashboard project showing mobile version"
-      />     
-      </div>
+          <Image
+            src="/cafetologo.jpeg"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />     
+          <Image
+            src="/cafetologo.jpeg"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshot of the dashboard project showing mobile version"
+          />     
+        </div>
       </div>
     </main>
   );
--- nextjs-dashboard/app/ui/dashboard/nav-links.tsx
+++ nextjs-dashboard/app/ui/dashboard/nav-links.tsx
@@ -1,22 +1,29 @@
+'use client';
+
 import {
-  UserGroupIcon,
   HomeIcon,
-  DocumentDuplicateIcon,
+  ShoppingCartIcon,
+  TruckIcon,
+  PhoneIcon,
 } from '@heroicons/react/24/outline';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
   {
-    name: 'Invoices',
-    href: '/dashboard/invoices',
-    icon: DocumentDuplicateIcon,
+    name: 'Productos',
+    href: '/dashboard/catalogue',
+    icon: ShoppingCartIcon,
   },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { name: 'Entregas', href: '/dashboard/deliveries', icon: TruckIcon },
+  { name: 'Contacto', href: '/dashboard/contact', icon: PhoneIcon },
 ];
 
 export default function NavLinks() {
+  const pathname = usePathname();
   return (
     <>
       {links.map((link) => {
@@ -25,8 +32,13 @@ export default function NavLinks() {
           <a
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
-          >
+            className={clsx(
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-brown-300 hover:text-brown-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              {
+                'bg-brown-100 text-brown-600': pathname === link.href,
+              },
+            )}
+            >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
           </a>
